932e8b1f7147dea4ea59ba7fcd066d85d11d9292
rgao02 <rgao02@syr.edu>
2023-11-12 01:21:23 -0500

    Initial Commit

---
+++ CampusScoreboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CampusScoreboard
+//
+//  Created by 高瑞鸿 on 11/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
